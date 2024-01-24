f590a8a8e74aa441a615fd7d0d6837810ee120a4
Jhon Jaime Corro Pareja <jcorro@horbathdev.com>
2024-01-24 18:18:55 -0500

    Add command line argument parsing to the solver script

--- robot.py
+++ robot.py
@@ -2,47 +2,48 @@ import os
 from pathlib import Path
 import shutil
 import subprocess
+import argparse
 
-INPUT_DIR = 'multicast_graphs'
-os.chdir(INPUT_DIR)
-
-OUTPUT_DIR = Path('solver_output')
-shutil.rmtree(OUTPUT_DIR)
-OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-
-SOLVER_NAME = 'multicast_graph_solver.exe'
+def get_cli_args():
+    parser = argparse.ArgumentParser('Run solver script')
+    parser.add_argument('input_dir', nargs='?', help='Directory with input files for the solver' )
+    parser.add_argument('solver_name', nargs='?', help='Solver script name with extension')
+    args = parser.parse_args()
+    return args
 
 def make_report(path):
     with open('report.csv', 'w') as report:
-        report.write('Grafo;Metodo de eliminacion;Representacion;Simple o Combinado\n')
+        report.write('Grafo;Metodo de eliminacion;Solucion\n')
         for file in path.iterdir():
-            text = file.name.replace('.out', '').split('_')
-            last_elem = text.pop(-1).split('-')
-            text[1] = f'{text[1]}_{last_elem.pop(0)}'
-            text.extend(last_elem)
+            graph_name, elimination_method, solution = file.name.replace('.out', '').split('-')[:-1]
+            solution = 'Simple' if solution == 'sim' else 'Combinado'
+            print(f'{graph};{elimination_method};{solution}')
+            report.write(f'{graph_name};{elimination_method};{solution}\n')
 
-            graph_name = text[0]
-            elimination_method = 'Primer camino' if text[1] == 'first_path' else 'Camino mas largo' if text[1] == 'longest_path' else 'Camino aleatorio'
-            simple_combination = 'Simple' if text[2] == 'sim' else 'Combinado'
-            representation = 'Letras' if text[3] == 'let' else 'Vectores binarios'
+if __name__ == '__main__':
+    args = get_cli_args()
 
-            print(f'{graph_name};{elimination_method};{representation};{simple_combination}')
-            report.write(f'{graph_name};{elimination_method};{representation};{simple_combination}\n')
+    INPUT_DIR = args.input_dir
+    SOLVER_NAME = args.solver_name
+    OUTPUT_DIR = Path('solver_output')
 
-options = ['0', '1']
-options_permutation = [[i, j] for i in options for j in options]
+    os.chdir(INPUT_DIR)
+    shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-multicast_graph_files = [f.name for f in Path('.').iterdir() if f.is_file() and f.suffix == '.txt']
-for graph in multicast_graph_files:
-    for option in options_permutation:
-        inputs = [graph, *option]
-        process = subprocess.Popen([SOLVER_NAME], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate(input='\n'.join(inputs).encode())
+    options = [['1', '0'], ['1', '1']]
 
-        if process.returncode != 0:
-            print(f"Error executing command: {stderr.decode()}")
+    multicast_graph_files = [f.name for f in Path('.').iterdir() if f.is_file() and f.suffix == '.txt']
+    for graph in multicast_graph_files:
+        for option in options:
+            inputs = [graph, *option]
+            process = subprocess.Popen([SOLVER_NAME], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate(input='\n'.join(inputs).encode())
 
-output_files = [f for f in Path('.').iterdir() if f.is_file() and f.suffix == '.out']
-list(map(lambda file: shutil.move(file, OUTPUT_DIR / file.name.replace('.txt', '')), output_files))
+            if process.returncode != 0:
+                print(f"Error executing command: {stderr.decode()}")
 
-make_report(OUTPUT_DIR)
+    output_files = [f for f in Path('.').iterdir() if f.is_file() and f.suffix == '.out']
+    list(map(lambda file: shutil.move(file, OUTPUT_DIR / file.name.replace('.txt', '')), output_files))
+
+    make_report(OUTPUT_DIR)
