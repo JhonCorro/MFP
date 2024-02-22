f644290acf718df1cd48b0ace86eb91163909e09
Jhon Jaime Corro Pareja <jcorro@horbathdev.com>
2024-02-22 01:00:36 -0500

    Fix issue with solver execution and move output files to designated directory

--- robot.py
+++ robot.py
@@ -33,27 +33,27 @@ if __name__ == '__main__':
     OUTPUT_DIR = Path('solver_output')
 
     os.chdir(INPUT_DIR)
-    # shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+    shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-    # options = [['1', '0'], ['1', '1']]
+    options = [['1', '0'], ['1', '1']]
 
-    # print('Running solver...')
+    print('Running solver...')
 
-    # multicast_graph_files = [f.name for f in Path('.').iterdir() if f.is_file() and f.suffix == '.txt']
-    # for graph in multicast_graph_files:
-    #     for option in options:
-    #         inputs = [graph, *option]
-    #         process = subprocess.Popen([SOLVER_NAME], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #         stdout, stderr = process.communicate(input='\n'.join(inputs).encode())
+    multicast_graph_files = [f.name for f in Path('.').iterdir() if f.is_file() and f.suffix == '.txt']
+    for graph in multicast_graph_files:
+        for option in options:
+            inputs = [graph, *option]
+            process = subprocess.Popen([SOLVER_NAME], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate(input='\n'.join(inputs).encode())
 
-    #         if process.returncode != 0:
-    #             print(f"Error executing command: {stderr.decode()}")
+            if process.returncode != 0:
+                print(f"Error executing command: {stderr.decode()}")
 
-    # print('Solver finished!')
+    print('Solver finished!')
 
-    # output_files = [f for f in Path('.').iterdir() if f.is_file() and f.suffix == '.out']
-    # list(map(lambda file: shutil.move(file, OUTPUT_DIR / file.name.replace('.txt', '')), output_files))
+    output_files = [f for f in Path('.').iterdir() if f.is_file() and f.suffix == '.out']
+    list(map(lambda file: shutil.move(file, OUTPUT_DIR / file.name.replace('.txt', '')), output_files))
 
     print('Writing report...')
     make_report(OUTPUT_DIR)
